0ad05b0129087fe2eec0bab480930ceb1c9cd681
name_loquito <you@example.com>
2023-07-18 22:53:08 -0500

    Balance.js bug solved decimals negative values

--- src/comp/Balance.js
+++ src/comp/Balance.js
@@ -1,19 +1,36 @@
 import { useContext } from "react"
 import { GlobalContext } from "../context/GlobalState"
 
+const FixNumber = (total) => { 
+  let x;
+
+  if(Number.isInteger(total)){
+    x = total
+  } else{
+    x = total.toFixed(2)
+  }
+
+  //this Fx fixe decimals an signal
+  let newTotal = x < 0 ? (
+    `${x.toString().slice(0,1)}$${x.toString().slice(1)}`
+  ) : `$${x}`;
+
+ return newTotal
+}
+
 const Balance = () => {
   const {transactions} = useContext(GlobalContext)
 
   //Extranct amounts
   const amounts = transactions.map(transaction => transaction.amount)
   const total = amounts.reduce((acc,item) => (acc += item),0)
-
+  
   return (
     <>
       <h4>Your Balance</h4>
-      <h1>${total}</h1>
+      <h1>{FixNumber(total)}</h1>
     </>
   )
 }
-
+ 
 export default Balance
